16beef200a5ab34fa94a001a928745da7de2e563
Vladislavv99 <vladlegur6@gmail.com>
2023-07-10 23:32:14 +0300

    add fanction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
 
 
 export const App = () => {
 
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+
+
+  const removeContact = (contactId) => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== contactId))
+  }
 
   return (
     <div>
-      <ContactForm/>
-    </div>
+      <h1>PhoneBook</h1>
+      <ContactForm />
+      <h1>Contacts</h1>
+      {contacts.length > 0 && <ContactList contacts={contacts} onRemoveContact={removeContact} />}
+    </div>  
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,11 @@
 
 
-const ContactList = () => {
+const ContactList = ({contacts, onRemoveContact}) => {
     return <div>
-        
+        <ul>
+            {contacts.map(contact => <li key={contact.id}>{contact.name}: {contact.number }
+            <button type="button" onClick={()=>onRemoveContact(contact.id)}>Delete</button></li>)}
+        </ul>
     </div>
 }
 
